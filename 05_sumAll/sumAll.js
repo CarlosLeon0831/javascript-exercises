5ee3b1d1955dd0f07421b90fd8b802d6e71a62aa
Carlos <carlosleon0831@gmail.com>
2022-07-28 11:10:58 -0500

    Solve fourth and fifth exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+let firstNumber = 1;
+let lastNumber = 4;
 
+const sumAll = function (firstNumber, lastNumber) {
+  let sum = 0;
+  for (let i = 0; i < lastNumber + 1; i++) {
+    sum += i;
+  }
+  return sum
 };
-
+console.log(sumAll(firstNumber, lastNumber));
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 == 0 && year % 100 != 0) {
+    return true;
+  }
+  if (year % 400 == 0){
+    return true;
+  } else {
+    return false;
+  }
 };
-
+leapYears(2400);
+console.log(leapYears(2400));
 // Do not edit below this line
 module.exports = leapYears;
